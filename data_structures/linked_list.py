2373bc6df4b2ccc705d5886e64a1e5c29329a594
ezequiel bugnon <bugnonezequiel@gmail.com>
2026-05-25 20:08:18 -0300

    first commit

---
+++ data_structures/linked_list.py
@@ -0,0 +1,26 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def append(self, value):
+        pass
+
+    def prepend(self, value):
+        pass
+
+    def insert(self,index, value):
+        pass        
+
+
+
+def create_linked_list_example():
+    my_linked_list = LinkedList(4)
+    print(f"my linked list: {my_linked_list}")
---
+++ main.py
@@ -0,0 +1,9 @@
+from data_structures.linked_list import create_linked_list_example
+
+def main():
+    print("Hello from pyton-mini-projects!")
+    create_linked_list_example()
+
+
+if __name__ == "__main__":
+    main()
